ff7e135a506bf1b3b71db91ddd3c2283360155e0
Michael Horne <mike@recantha.co.uk>
2014-07-01 08:08:17 +0000

    Add comments to explain code

--- ultramin.py
+++ ultramin.py
@@ -1,3 +1,10 @@
+# Raspberry Pi Theramin-like musical instrument
+# Written by Michael Horne
+# 1st July 2014
+# Feel free to take and adapt the code. A reference back to my blog at
+# http://www.recantha.co.uk/blog would be appreciated if you publish it.
+
+# Import your libraries
 import RPi.GPIO as GPIO
 import time
 import threading
@@ -7,34 +14,47 @@ import threading
 PIN_TRIG=23
 PIN_ECHO=24
 
-# Buzzer, including initial frequencies
-# Remember, the buzzing is going to be threaded so we need to set the FREQUENCY as a constant that will be changed by the ultrasonic reading
+# Define buzzer, including initial frequencies
+# We set the buzzer going straight away so we have to set an initial frequency
+# We also set a multiplier for the frequency so we can have bigger range of tones
 PIN_BUZZER = 18
 BASE_FREQUENCY = 300
 FREQUENCY_MULTIPLIER = 2
 
+# Function to read ultrasonic sensor
 def read_ultrasonic():
+	# Switch the trigger pin on and off very quickly
+	# This is what the sensor expects and tells it to send the pulse
 	GPIO.output(PIN_TRIG, True)
 	time.sleep(0.00001)
 	GPIO.output(PIN_TRIG, False)
 
+	# Wait for the echo pin to go low (i.e. it's waiting)
 	signal_off = 0
 	signal_on = 0
 	while GPIO.input(PIN_ECHO) == 0:
 		signal_off = time.time()
 
+	# Wait for the echo pin to go high (i.e. it's received the ping)
 	while GPIO.input(PIN_ECHO) == 1:
 		signal_on = time.time()
 
+	# Work out how long it took for the ping to go out and back
 	time_passed = signal_on - signal_off
 
+	# Speed of sound calculation to work out distance
 	distance = time_passed * 34000
+
 	# That was the distance there and back so halve it to get true distance
 	distance = distance/2
+
+	# Print the distance out. You CAN comment this out if you don't care
+	# to see the distance read-out
 	print distance
 
 	return distance
 
+# Some things it's just nicer to have inside a function
 def setup():
 	# Set-up GPIO
 	GPIO.setwarnings(False)
@@ -50,15 +70,19 @@ def setup():
 	# Set-up buzzer
 	GPIO.setup(PIN_BUZZER, GPIO.OUT)
 
+# Wrapper to change frequency of buzzer via PWM
 def change_frequency(freq):
 	p.ChangeFrequency(freq)
 
 ######################################################
 
+# Run set-up and set-up the PWM for the buzzer
 setup()
 p = GPIO.PWM(PIN_BUZZER,BASE_FREQUENCY)
 p.start(1)
 
+# Infinite loop to read distance and change frequency of buzzer
+# based on the reading
 try:
 	while True:
 		distance = read_ultrasonic()
@@ -66,6 +90,8 @@ try:
 		change_frequency(BASE_FREQUENCY+(distance*FREQUENCY_MULTIPLIER))
 
 except KeyboardInterrupt:
+	# Ctrl-C exits the program here
 	pass
 
+# Don't forget to cleanup otherwise the buzzer won't stop!
 GPIO.cleanup()
